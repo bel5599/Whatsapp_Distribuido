59d0020ac1ee86986b50e5fba48702c8a2da4f3f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-05-30 23:26:14 -0400

    add RemoteNode class, proxy of Node in the network

---
+++ src/chord/remote_node.py
@@ -0,0 +1,96 @@
+import zmq
+
+from chord.zmq_context import CONTEXT
+from message import Message, is_valid_response
+
+
+class GetRemoteNodeMessage(Message):
+    def __init__(self, type):
+        super().__init__(type)
+
+    def read(self, json_dict: dict):
+        response_type = json_dict.get("type", "")
+        success = json_dict.get("success", False)
+
+        if is_valid_response(response_type, self._type) and success:
+            payload = json_dict.get("payload", {})
+            return RemoteNode.from_dict(payload)
+
+
+class SetRemoteNodeMessage(Message):
+    def __init__(self, type: str):
+        super().__init__(type)
+
+    def read(self, json_dict: dict):
+        response_type = json_dict.get("type", "")
+        success = json_dict.get("success", False)
+
+        return is_valid_response(response_type, self._type) and success
+
+
+class RemoteNode:
+    GET_SUCCESSOR = GetRemoteNodeMessage("get_successor")
+    GET_PREDECESSOR = GetRemoteNodeMessage("get_predecessor")
+    SET_PREDECESSOR = SetRemoteNodeMessage("set_predecessor")
+
+    def __init__(self, id: int, address: str, port: str):
+        self.id = id
+        self.address = address
+        self.port = port
+
+    def _remote_call(self, payload, message: Message, timeout=2500, retries=3):
+        endpoint = f"tcp://{self.address}:{self.port}"
+
+        data = message.write(payload)
+
+        client = CONTEXT.socket(zmq.REQ)
+        client.connect(endpoint)
+        client.send_json(data)
+
+        while retries > 0:
+            if (client.poll(timeout) & zmq.POLLIN) != 0:
+                reply = message.read(client.recv_json())
+                if isinstance(reply, dict):
+                    reply_data = message.read(reply)
+                    if reply_data != None:
+                        return reply_data
+
+            retries -= 1
+            client.setsockopt(zmq.LINGER, 0)
+            client.close()
+
+            client = CONTEXT.socket(zmq.REQ)
+            client.connect(endpoint)
+            client.send_json(data)
+
+    @property
+    def succesor(self):
+        return self._remote_call({}, self.GET_SUCCESSOR)
+
+    @property
+    def predecessor(self):
+        return self._remote_call({}, self.GET_PREDECESSOR)
+
+    @property.setter
+    def predecessor(self, remote_node):
+        return self._remote_call(remote_node.to_dict(), self.SET_PREDECESSOR)
+
+    def update_finger_table(self, remote_node, index: int):
+        # comunicacion por la red
+        pass
+
+    def find_successor(self, id):
+        # comunicacion por la red
+        pass
+
+    def to_dict(self):
+        return {"id": self.id, "address": self.address, "port": self.port}
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        id = d.get("id", None)
+        address = d.get("address", None)
+        port = d.get("port", None)
+
+        if id != None and address != None and port != None:
+            return cls(id, address, port)
--- src/chord/tools.py
+++ src/chord/tools.py
@@ -1,16 +1,20 @@
 import random
+from typing import Union
+
+from chord.remote_node import RemoteNode
+
 
 class Finger:
-    def __init__(self, m, k, node = None):
-        self.start = (2**k) % (2**m) # 2^(k) mod 2^m
-        self.end =  (2**(k+1)) % (2**m) #2^(k+1) mod 2^m
+    def __init__(self, m, k, node: Union[RemoteNode, None] = None):
+        self.start = (2**k) % (2**m)  # 2^(k) mod 2^m
+        self.end = (2**(k+1)) % (2**m)  # 2^(k+1) mod 2^m
         self.node = node
         # finger[i].node = suc cessor(finger[i].start)
 
 
 class Node:
     def __init__(self, id: int, dir, port, keys, storage, transport, m):
-        
+
         self.dir = dir
         self.port = port
         self.id = id
@@ -22,7 +26,7 @@ class Node:
         self.finger_table = [Finger(m, k) for k in range(m)]
 
         self.storage = storage
-        self.transport = transport 
+        self.transport = transport
         self.m = m
 
     @property
@@ -30,7 +34,7 @@ class Node:
         return self.finger_table[0].node
 
     @property.setter
-    def succesor(self, node_info):
+    def succesor(self, remote_node: RemoteNode):
         pass
 
     @property
@@ -38,7 +42,7 @@ class Node:
         return self._predecessor
 
     @property.setter
-    def predecessor(self, node_info):
+    def predecessor(self, remote_node: RemoteNode):
         pass
 
     def closest_preceding_finger(self, id: int):
@@ -66,10 +70,10 @@ class Node:
         # else:
         #     for finger in self.finger_table[::-1]:
         #         finger.node.id = self.id
-            
 
     def init_finger_table(self, node):
-        self.fingerTable[1].node = node.find_successor(self.fingerTable[1].start)
+        self.fingerTable[1].node = node.find_successor(
+            self.fingerTable[1].start)
         successor = self.fingerTable[1].node
         predecessor = successor.predecessor
         successor.predecessor = node
@@ -78,11 +82,12 @@ class Node:
             if self.fingerTable[i+1].start in (self, self.fingerTable[i].node):
                 self.fingerTable[i+1].node = self.fingerTable[i].node
             else:
-                self.fingerTable[i+1].node = node.findSuccessor(self.fingerTable[i+1].start)
+                self.fingerTable[i +
+                                 1].node = node.findSuccessor(self.fingerTable[i+1].start)
 
     def update_others(self):
         for i in range(1, self.m):
-            pred = self.findPredecessor(2**m - 2**(i-1))# n - 2^(i-1)
+            pred = self.findPredecessor(2**m - 2**(i-1))  # n - 2^(i-1)
             pred.updateFingerTable(self, i)
 
     def update_finger_table(self, node, i):
@@ -104,35 +109,3 @@ class Node:
     # def fix_fingers(self):
     #     i = random.Random()
     #     self.fingerTable[i].node = self.findSuccessor(self.fingerTable[i].start)
-
-class NodeInfo:
-    def __init__(self, id: int, dir, port):
-        self.id = id
-        self.dir = dir
-        self.port = port
-
-    property
-    def succesor(self):
-        pass
-
-    @property.setter
-    def succesor(self, node_info):
-        pass
-
-    @property
-    def predecessor(self):
-        pass
-
-    @property.setter
-    def predecessor(self, node_info):
-        pass
-
-    def update_finger_table(self, node_info, i: int):
-        #comunicacion por la red
-        pass
-
-    def find_successor(self, id):
-        #comunicacion por la red
-        pass
-
-    
