fec77ea823594718939e1b2f47468853f249bf0b
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-05-30 23:54:46 -0400

    add app commands

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main():
+from typer import Typer
+from typing import Union, Literal
+
+
+NodeType = Union[Literal["client"], Literal["entity-manager"]]
+
+
+app = Typer()
+
+
+@app.command
+def create(port: str, capacity: int = 64, node: NodeType = "entity-manager"):
+    capacity = max(capacity, 256)
+    pass
+
+
+@app.command
+def join(address: str, node: NodeType = "client"):
+    ip, port = address.split(':')
     pass
 
 
 if __name__ == "__main__":
-    main()
+    app()
