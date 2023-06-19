be30062717110dfcf2b98b181f3de3eca4e15f59
bel5599 <95463994+bel5599@users.noreply.github.com>
2023-06-19 12:46:58 -0600

    act class router

---
+++ src/router/router.py
@@ -0,0 +1,29 @@
+
+class Router:
+    def __init__(self):
+        self.handlers = {}
+
+    def add_handler(self, handler, payload_reader, payload_writer):
+
+        if not handler in self.handlers:
+            self.handlers[handler] = (payload_reader, payload_writer)
+
+        return self
+
+    # def remove_handler(self, handler):
+    #     self.handler.pop(handler, None)
+    #     return self
+
+    def route(self, request_data:dict):
+
+        type = request_data.get("type", None)
+
+        if type is not None:
+            t = self.handlers.get(type, None)
+            if t is not None:
+                reader, writer = t
+                request = reader.read(request_data)
+                if request.is_valid():
+                    response_data = writer.write(request.data)
+                    return (request.ip, request.port, response_data)
+                        
