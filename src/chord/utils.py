872610d280513f0b6a6ecbff607ae7cb0a662d0f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-19 14:16:51 -0400

    impl get_requester

--- src/chord/utils.py
+++ src/chord/utils.py
@@ -1,5 +1,16 @@
 from hashlib import sha256
+from os import environ
+
+PORT_KEY = "REQUESTER_PORT"
+IP_KEY = "REQUESTER_IP"
 
 
 def generate_id(text: str, bits=64):
     return int(sha256(text.encode()).hexdigest(), 16) % 2**bits
+
+
+def get_requester():
+    # si no hay internet, obtener el port de otra forma
+    ip = environ.get(IP_KEY)
+    port = environ.get(PORT_KEY)
+    return {"ip": ip, "port": port}
