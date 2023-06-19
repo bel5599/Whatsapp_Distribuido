0e223be43808c87abd4ea7e05ce4b61db2139933
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-19 13:30:26 -0400

    import from router & mark _remote_call as not impl

--- src/chord/remote_node.py
+++ src/chord/remote_node.py
@@ -2,33 +2,12 @@ import zmq
 
 from chord.base_node import BaseNode
 from chord.zmq_context import CONTEXT
+from src.router import Request, RequestWriter, ResponseReader
 
 
 class RemoteNode(BaseNode):
-    def _remote_call(self, payload, message, timeout=2500, retries=3):
-        endpoint = f"tcp://{self.ip}:{self.port}"
-
-        data = message.write(payload)
-
-        client = CONTEXT.socket(zmq.REQ)
-        client.connect(endpoint)
-        client.send_json(data)
-
-        while retries > 0:
-            if (client.poll(timeout) & zmq.POLLIN) != 0:
-                reply = message.read(client.recv_json())
-                if isinstance(reply, dict):
-                    reply_data = message.read(reply)
-                    if reply_data != None:
-                        return reply_data
-
-            retries -= 1
-            client.setsockopt(zmq.LINGER, 0)
-            client.close()
-
-            client = CONTEXT.socket(zmq.REQ)
-            client.connect(endpoint)
-            client.send_json(data)
+    def _remote_call(self, request: Request, timeout=2500, retries=3):
+        raise NotImplementedError()
 
     def successor(self):
         # comunicacion por la red
