49e2f654bc71585c08ce4a61a4c6c2d9076f2f2c
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-24 12:12:59 -0400

    impl /predecessor/* api calls

---
+++ server/chord/routers/predecessor.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request
+
+
+from ..base_node import BaseNode, BaseNodeModel
+
+
+router = APIRouter(prefix="/predecessor", tags=["predecessor"])
+
+
+@router.get("/")
+async def get_predecessor(request: Request):
+    node: BaseNode = request.state.node
+
+    result_node = await node.predecessor()
+    return result_node.serialize()
+
+
+@router.put("/")
+async def set_predecessor(model: BaseNodeModel, request: Request):
+    node: BaseNode = request.state.node
+    new_node = BaseNode.from_base_model(model)
+
+    await node.set_predecessor(new_node)
+
+    return new_node.serialize()
