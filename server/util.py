2079b103b0b11cee981c8c8c13299ed85062d2a7
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-26 15:13:01 -0400

    get_ip return local ip if local

--- server/util.py
+++ server/util.py
@@ -2,19 +2,25 @@ import socket
 from hashlib import sha256
 
 
+LOCAL_IP = "127.0.0.1"
+
+
 def generate_id(text: str, bits=64):
     return int(sha256(text.encode()).hexdigest(), 16) % 2**bits
 
 
-def get_ip():
+def get_ip(local=False):
     ip = ""
 
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-        s.close()
-    except:
-        ip = "127.0.0.1"
+    if local:
+        ip = LOCAL_IP
+    else:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            ip = s.getsockname()[0]
+            s.close()
+        except:
+            ip = LOCAL_IP
 
     return ip
