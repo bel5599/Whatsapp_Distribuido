43dab68a2514ba5a7c759b7a69e2069d11c56545
bel5599 <95463994+bel5599@users.noreply.github.com>
2023-06-27 19:35:54 -0600

    actualizacion en la clase remote_entity_node

--- server/node/remote_entity_node.py
+++ server/node/remote_entity_node.py
@@ -1,6 +1,92 @@
+from requests import get, put, delete
+
 from ..chord.remote_node import RemoteNode as ChordRemoteNode
 
 class RemoteEntityNode(ChordRemoteNode):
     def __init__(self, id: int, ip: str, port: str):
         super().__init__(id, ip, port)
+        self.url = f"http://{self.ip}:{self.port}"
+
+    def add_user(self):
+        response = put(f"{self.url}/user/add")
         
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+    
+    def delete_user(self, nickname: str):
+        response = delete(f"{self.url}/user/delete/{nickname}")
+        
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+    
+    def add_messenger(self):
+        response = put(f"{self.url}/messenger/add")
+        
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+    
+    def delete_messenger(self, nickname: str):
+        response = delete(f"{self.url}/messenger/delete/{nickname}")
+        
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+    
+    def search_messenger_from(self):
+        response = get(f"{self.url}/messenger/from")
+
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+
+    def search_messenger_to(self):
+        response = get(f"{self.url}/messenger/to")
+
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+
+    def add_chat(self):
+        response = put(f"{self.url}/chat/add")
+        
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+
+    def search_chat_id(self):
+        response = get(f"{self.url}/chat")
+
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+
+    def delete_chat(self):
+        response = delete(f"{self.url}/chat/delete")
+        
+        if response.status_code == 200:
+            result: dict = response.json()
+            return result
+        
+        raise Exception(response.json()["detail"])
+
+    
+    
