c63a40bd8d829f5de227b9309d94198bcf38d441
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-02 20:24:09 -0400

    add request manager

---
+++ reqs/__init__.py
@@ -0,0 +1,22 @@
+from requests import get, put
+from json import dumps
+
+
+class RequestManager:
+    def __init__(self, ip: str, port: str, headers: dict[str, str] = {}, secure=False):
+        s = "s" if secure else ""
+        self._url = f"http{s}://{ip}:{port}"
+        self._headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            **headers
+        }
+
+    def get(self, route: str, **kwargs):
+        return get(f"{self._url}{route}", **kwargs)
+
+    def put(self, route: str, **kwargs):
+        data = kwargs.get("data", {})
+        kwargs["data"] = dumps(data)
+
+        return put(f"{self._url}{route}", **kwargs)
