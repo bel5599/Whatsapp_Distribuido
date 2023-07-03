24d4c43a1f3c875f9998a95ebd319bcd6b8effb1
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-03 16:42:43 -0400

    impl heartbeats service app

--- heartbeat_app.py
+++ heartbeat_app.py
@@ -1,2 +1,33 @@
 if __name__ == "__main__":
-    pass
+    import asyncio
+    from typer import Typer
+    from uvicorn import Config, Server
+
+    from service.heartbeat.app import app as fastapi_app, manager
+    from shared import get_ip
+
+    typer_app = Typer()
+
+    @typer_app.command()
+    def up(port: str = "4173", interval: int = 10, local: bool = False):
+        ip = get_ip(local)
+
+        async def _serve():
+            config = Config(fastapi_app, host=ip, port=int(port))
+            server = Server(config)
+            await server.serve()
+
+        async def _start_service():
+            await asyncio.sleep(1)
+            # manager hace algo para empezar a avisar
+            # cada cierto tiempo (usar interval)
+            pass
+
+        async def _gather():
+            s = _serve()
+            h = _start_service()
+            await asyncio.gather(s, h)
+
+        asyncio.run(_gather())
+
+    typer_app()
