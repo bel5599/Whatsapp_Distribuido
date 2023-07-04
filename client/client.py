83e23585a3f2f795250948ec0ee0d3ecf4b99aaf
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-04 00:01:09 -0600

    update client

--- client/client.py
+++ client/client.py
@@ -11,6 +11,23 @@ from .client_node import ClientNode
 client_interface = FastAPI()
 client = ClientNode()
 
+def replication_user(inf_nodo,nickname,password):
+    # nodo que va a guardar la informacion del user
+    server_node_data = RemoteEntityNode(-1, inf_nodo['ip'], inf_nodo['port'])
+    server_node_data.add_user(nickname,password)
+    # replicar la informacion del usuario en el nodo sucesor
+    dict_successor = server_node_data.successor()
+    server_successor = RemoteEntityNode(-1,dict_successor.ip,dict_successor.port)
+    server_successor.add_user(nickname,password)
+    # replicar la informacion del usuario en el nodos antecesor
+    dict_predecessor = server_node_data.predecessor()
+    server_predecessor = RemoteEntityNode(-1,dict_predecessor.ip,dict_predecessor.port)
+    server_predecessor.add_user(nickname,password)
+    
+    return dict_successor,dict_predecessor
+
+def replication_messenge(inf_nodo,user,messenge):
+    pass
 
 @client_interface.post("/Register")
 def register(nickname: str, password: str, server: str):
@@ -26,19 +43,8 @@ def register(nickname: str, password: str, server: str):
     else: # Hashear el nickname para obtener un servidor
         node_data = server_node.search_entity_node(nickname)
         
-    # Guardar la informacion del usuario
-    # nodo que va a guardar la informacion del user
-    server_node_data = RemoteEntityNode(-1, node_data['ip'], node_data['port'])
-    server_node_data.add_user(nickname,password)
-    # replicar la informacion del usuario en el nodo sucesor
-    dict_successor = server_node_data.successor()
-    server_successor = RemoteEntityNode(-1,dict_successor.ip,dict_successor.port)
-    server_successor.add_user(nickname,password)
-    # replicar la informacion del usuario en el nodos antecesor
-    dict_predecessor = server_node_data.predecessor()
-    server_predecessor = RemoteEntityNode(-1,dict_predecessor.ip,dict_predecessor.port)
-    server_predecessor.add_user(nickname,password)
-    
+    # Guardar la informacion del usuario y replica la informacion
+    dict_successor,dict_predecessor = replication_user(node_data,nickname,password)
     # Agrega al entity que guarda los datos del cliente, su antecesor, sucesor y por el q se conecta
     servers =[]
     servers.append(server)
@@ -113,7 +119,7 @@ def messages(nickname: str):  # usuario de la conversacion conmigo
     node_data = RemoteEntityNode(-1, ip, port) 
     
     if search: #entonces hay que buscar el nodo con iformacion del usuario
-        node_data = node_data.nickname_entity_node(nickname)
+        node_data = node_data.nickname_entity_node(client.user)
         #si no se encontro un nodo con informacion del usuario
         if node_data.get('ip') is None:
             return "Your data has been lost"
@@ -136,12 +142,43 @@ def messages(nickname: str):  # usuario de la conversacion conmigo
 # para enviar mensajes a otro usuario
 @client_interface.post("/Send")
 def send(user: str, message: str):
-    # se chequea que yo esté loggeado
+    # se chequea que el usuario esté loggeado
+    if not client.login:
+        return "You are not logged in"
+    # buscar el entity en que está almacenada la información del usuario desde su lista de servidores
+    servers = client.server
+    # server que contiene su informacion original
+    if len(servers)>1:
+        inf_node = servers[1]
+    else:        
+    # en caso de que no haya activo o guardado un nodo que tenga su informacion
+        inf_node = client.server[0]
+        search = True
     
-    # retornar una notificacion en caso de no estarlo
-    # buscar el entity en que está almacenada la información del usuario
-    # busco mi lista de contactos
+    ip = inf_node.split(':')[0]
+    port = inf_node.split(':')[1]
+    node_data = RemoteEntityNode(-1, ip, port) 
+    
+    if search: #entonces hay que buscar el nodo con iformacion del usuario
+        node_data = node_data.nickname_entity_node(client.user)
+        #si no se encontro un nodo con informacion del usuario
+        if node_data.get('ip') is None:
+            return "Your data has been lost"
+        # si se encontro un nodo, obtener el nodo para hcerle los pedidos
+        node_data = RemoteEntityNode(-1, node_data['ip'], node_data['port']) 
+        
     # buscar el entity en que está almacenada la información del otro usuario
-    # si mi server está activo le mando el mensaje para ser escrito
-    # si su servidor está activo y no es mi mismo servidor hago lo mismo. Si somos del mismo server ya su info se escribió
+    other_user = node_data.nickname_entity_node(user)
+    
+    # FALTA VERIFICACION DE SI ESTA ACTIVO O NO UN SERVIDOR 
+    # si la informacion de los usuarios no se guarda en la misma base datos, gurdarlo en la de el tambien
+    if other_user['ip']!= node_data['ip'] or other_user['port']!= node_data['port']:
+        other_user = RemoteEntityNode(-1, other_user['ip'], other_user['port'])
+        # si su servidor está activo y no es mi mismo servidor hago lo mismo. Si somos del mismo server ya su info se escribió
+        other_user.add_messenger(client.user,user,message) 
+    
+    # si mi server está activo le mando el mensaje para ser escrito    
+    node_data.add_messenger(client.user,user,message)
+    # Replicacion de los datos
+    
     return
