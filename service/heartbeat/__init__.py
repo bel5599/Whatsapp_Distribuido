fb6fe98da32193f34dad2e6cfc8ebb887a327cb2
bel5599 <95463994+bel5599@users.noreply.github.com>
2023-07-04 16:58:01 -0600

    corregido el posible error en la clase heartbeatManager

--- service/heartbeat/__init__.py
+++ service/heartbeat/__init__.py
@@ -12,24 +12,15 @@ class HeartBeatManager:
 
     def check_health(self, interval: int):
         while True:
+            temp_set = set()
             for request_manager in self.request_manager_list:
                 try:
                     response = request_manager.get("/heart")
                 except:
-                    self.request_manager_list.remove(request_manager)
+                    temp_set.add(request_manager)
                 else:
                     if not response.status_code == 200:
-                        self.request_manager_list.remove(request_manager)
+                        temp_set.add(request_manager)
+            self.request_manager_list = self.request_manager_list - temp_set
 
             sleep(interval)
-
-# TODO:
-# 1- request_manager_list sea un set de python
-# 2- en lugar de hacer el for y ya, necesito que
-# cojas todas los request manager que:
-#   1- o dieron status_code != 200
-#   2- o dieron error
-# asumes q esos nodos se desconectaron y por tanto
-# remuevelos de la lista (que seria un set)
-# aqui tienes la documentacion de los set https://docs.python.org/3.9/library/stdtypes.html#set
-# ;)
