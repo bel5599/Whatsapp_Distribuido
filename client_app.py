e5954369405fd1401efe00f8821fa5038b5d8009
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-05 16:21:56 -0600

    Update client_app.py

--- client_app.py
+++ client_app.py
@@ -2,15 +2,23 @@
 # levanta la interfaz del cliente
 if __name__ == "__main__":
     import uvicorn
+    import threading
+    import time
     from typer import Typer
-    from shared import get_ip
     from client.client import client_interface,client
 
     typer_app = Typer()
 
     @typer_app.command()
     def run(ip:str,port:int =9000):
+        # Creacion de un hilo para este servicio
+        def update():
+            time.sleep(1)
+            client.update_servers()
+        stabilize_task = threading.Thread(target=update)    
+        # Levantar el hilo
+        stabilize_task.start()
+        # Correr el cliente
         uvicorn.run(client_interface,host=ip, port=port)
-        client.update_servers()
-
+        
     typer_app()
