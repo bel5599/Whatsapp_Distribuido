77b23c113782de11f11565c5bfb94c4158f2fc5b
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-06 23:50:48 -0600

    Update main.py

--- data/main.py
+++ data/main.py
@@ -1,6 +1,6 @@
 #from database import DataBase
 from database_client import DataBaseClient
-from database_user import DataBaseUser
+from database_entity import DataBaseUser
 
 
 
@@ -10,11 +10,15 @@ if __name__ == '__main__':
     data1 = DataBaseClient('data1')
     #data1.add_user('Ale','1234')
     # data1.add_user('Fernanda','1234')
-    data1.add_messenger('Fernanda','Ale','Hola')
+    #data1.add_messenger('Fernanda','Ale','Hola')
     # data1.add_messenger('Ale','Fernanda','Hola')
-
-    data2 = DataBaseUser('data2')
-    data2.add_user('Ale','1234','12344444','9000')
+    data1.add_contacts('Daniela','Ale',"el bobo")
+    data1.add_contacts('Roxana','Ale',"el bobo")
+    
+    data1.delete_contact('Daniela','Ale')
+    #data1.add_contacts('Daniela','Ale',"el bobo")
+    #data2 = DataBaseUser('data2')
+    #data2.add_user('Ale','1234','12344444','9000')
     # data2.add_user('Roxana','1234')
     # data2.add_messenger('Fernanda','Ale','Hola')
     # data2.add_messenger('Ale','Fernanda','Hola')
