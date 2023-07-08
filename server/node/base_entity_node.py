abedf7def70220830f01a352ce7a943d92a0f25d
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-08 12:50:53 -0600

    Update base_entity_node.py

--- server/node/base_entity_node.py
+++ server/node/base_entity_node.py
@@ -5,32 +5,34 @@ from ...data.database_entity import DataBaseUser
 
 
 class BaseEntityNode(BaseNode):
-    def add_user(self, nickname: str, pasword: str, ip: str, port: str, database_original: bool) -> bool:
+    def add_user(self, nickname: str, pasword: str, ip: str, port: str, database_id: int=-1) -> bool:
         raise NotImplementedError()
 
-    def get_pasword(self, nickname: str, database_original: bool) -> str:
+    def get_pasword(self, nickname: str, database_id: int=-1) -> str:
         raise NotImplementedError()
 
-    def nickname_entity_node(self, nickname: str, database_original: bool) -> Union["BaseEntityNode", None]:
+    def delete_user(self, nickname: str, database_id: int=-1) -> bool:
+        raise NotImplementedError()
+
+    
+    def nickname_entity_node(self, nickname: str, database_id: int=-1) -> Union["BaseEntityNode", None]:
         raise NotImplementedError()
 
     def search_entity_node(self, nickname: str) -> Union["BaseEntityNode", None]:
         raise NotImplementedError()
 
-    def delete_user(self, nickname: str, database_original: bool) -> bool:
+    
+    def add_messenger(self, source: str, destiny: str, value: str, database_id: int=-1) -> bool:
         raise NotImplementedError()
 
-    def add_messenger(self, source: str, destiny: str, value: str, database_original: bool) -> bool:
+    def search_messenger_from(self, me: str, user: str, database_id: int=-1) -> list:
         raise NotImplementedError()
 
-    def search_messenger_from(self, me: str, user: str, database_original: bool) -> list:
+    def search_messenger_to(self, me: str, user: str, database_id: int=-1) -> list:
         raise NotImplementedError()
 
-    def search_messenger_to(self, me: str, user: str, database_original: bool) -> list:
+    def delete_messenger(self, id_messenger: int, database_id: int=-1) -> bool:
         raise NotImplementedError()
 
-    def delete_messenger(self, id_messenger: int, database_original: bool) -> bool:
-        raise NotImplementedError()
-
-    def copy_database(self, source: DataBaseUser, database_id: int) -> None:
+    def copy_database(self, source: DataBaseUser, database_id: int=-1) -> None:
         raise NotImplementedError()
