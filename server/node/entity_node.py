4d735a8a6ef059985862f467447cc8d33ea903d1
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-08 10:26:05 -0400

    refactor methods and add TODO to others

--- server/node/entity_node.py
+++ server/node/entity_node.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Union
 
-from ..chord.node import Node as ChordNode
 from data.database_entity import DataBaseUser
+from ..chord.node import Node as ChordNode
 from ..util import generate_id
 from .base_entity_node import BaseEntityNode
 
@@ -29,67 +30,102 @@ class SearchMessengerModel(BaseModel):
     source: str
     destiny: str
     database_id: int
-    
+
 
 class CopyDataBaseModel(BaseModel):
     source: DataBaseUser
     database_id: int
 
 
+class DatabaseReplica:
+    def __init__(self, owner: Union[BaseEntityNode, None], db: Union[DataBaseUser, None]):
+        self.owner = owner
+        self.db = db
+
+
 class EntityNode(ChordNode, BaseEntityNode):
     def __init__(self, ip: str, port: str, capacity: int):
         super().__init__(ip, port, capacity)
-        self.database = DataBaseUser("data")
-        # self.replication_database = DataBaseUser("replication_data")
 
-        self.predecessor_replica = (self.predecessor().id, DataBaseUser("replication_data"))
-        self.second_predecessor_replica = (self.predecessor().predecessor.id, DataBaseUser("replication_data"))
+        self.database = DataBaseUser("data")
+
+        # get two predecessors and make sure both are different from self
+
+        predecessor = self.predecessor()
+        if predecessor and predecessor == self:
+            predecessor = None
+
+        second_predecessor = predecessor and predecessor.predecessor()
+        if second_predecessor and second_predecessor == self:
+            second_predecessor = None
+
+        # then, create replicas
+        self.replicas = [
+            DatabaseReplica(predecessor, predecessor and DataBaseUser(
+                "pred_replication_data")),
+            DatabaseReplica(second_predecessor, second_predecessor and DataBaseUser(
+                "secondpred_replication_data"))
+        ]
+
+        # self.predecessor_replica = (
+        #     self.predecessor().id, DataBaseUser("replication_data"))
+        # self.second_predecessor_replica = (
+        #     self.predecessor().predecessor.id, DataBaseUser("replication_data"))
+
+    def _get_database(self, database_id: int = -1):
+        if database_id == -1:
+            return self.database
+
+        for replica in self.replicas:
+            owner = replica.owner
+            if owner and owner.id == database_id:
+                return replica.db
 
     # User
     def add_user(self, nickname: str, password: str, ip: str, port: str, database_id: int):
-        if database_id == -1:
-            return self.database.add_user(nickname, password, ip, port)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].add_user(nickname, password, ip, port)
-        return self.second_predecessor_replica[1].add_user(nickname, password, ip, port)
+        db = self._get_database(database_id)
+        if db:
+            return db.add_user(nickname, password, ip, port)
+
+        return False
 
     def get_users(self, database_id: int):
-        if database_id == -1:
-            return self.database.get_users()
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].get_users()
-        return self.second_predecessor_replica[1].get_users()
+        db = self._get_database(database_id)
+        if db:
+            return db.get_users()
+
+        return False
 
     def get_pasword(self, nickname: str, database_id: int):
-        if database_id == -1:
-            return self.database.get_password(nickname)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].get_password(nickname)
-        return self.second_predecessor_replica[1].get_password(nickname)
+        db = self._get_database(database_id)
+        if db:
+            return db.get_password(nickname)
+
+        # TODO: determinar el return type de db.get_password
+        return False
 
     def delete_user(self, nickname: str, database_id: int):
-        if database_id == -1:
-            return self.database.delete_user(nickname)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].delete_user(nickname)
-        return self.second_predecessor_replica[1].delete_user(nickname)
+        db = self._get_database(database_id)
+        if db:
+            return db.delete_user(nickname)
+
+        return False
 
     def update_user(self, nickname: str, ip: str, port: str, database_id: int):
-        if database_id == -1:
-            return self.database.update_user(nickname, ip, port)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].update_user(nickname, ip, port)
-        return self.second_predecessor_replica[1].update_user(nickname, ip, port)
-    
-    def get_ip_port(self,nickname: str,database_id: int=-1):
-        if database_id == -1:
-            return self.database.get_ip_port(nickname)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].get_ip_port(nickname)
-        return self.second_predecessor_replica[1].get_ip_port(nickname)
+        db = self._get_database(database_id)
+        if db:
+            return db.update_user(nickname, ip, port)
 
+    def get_ip_port(self, nickname: str, database_id: int = -1):
+        db = self._get_database(database_id)
+        if db:
+            return db.get_ip_port(nickname)
+
+        # TODO: determinar el return type de db.get_ip_port
+        return None
 
     def nickname_entity_node(self, nickname: str, database_id: int):
+        # TODO: explicar que hace esta funcion
         if database_id == -1:
             self.database.contain_user(nickname)
             return self
@@ -99,6 +135,7 @@ class EntityNode(ChordNode, BaseEntityNode):
         return self.successor.nickname_entity_node_rec(nickname, self, database_id)
 
     def nickname_entity_node_rec(self, nickname: str, node, database_id: int):
+        # TODO: explicar que hace esta funcion
         if self.id == node.id:
             return None
 
@@ -116,33 +153,35 @@ class EntityNode(ChordNode, BaseEntityNode):
     # MESSENGES
 
     def add_messenges(self, source: str, destiny: str, value: str, database_id: int):
-        if database_id == -1:
-            return self.database.add_messenges(source, destiny, value)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].add_messenges(source, destiny, value)
-        return self.second_predecessor_replica[1].add_messenges(source, destiny, value)
+        db = self._get_database(database_id)
+        if db:
+            return db.add_messenges(source, destiny, value)
+
+        return False
 
     def search_messenges_from(self, me: str, user: str, database_id: int):
-        if database_id == -1:
-            return self.database.search_messenges_from(me, user)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].search_messenges_from(me, user)
-        return self.second_predecessor_replica[1].search_messenges_from(me, user)
+        db = self._get_database(database_id)
+        if db:
+            return db.search_messenges_from(me, user)
+
+        # TODO: determinar el return type de db.search_messenges_from
+        return None
 
     def search_messenges_to(self, me: str, user: str, database_id: int):
-        if database_id == -1:
-            return self.database.search_messenges_to(me, user)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].search_messenges_to(me, user)
-        return self.second_predecessor_replica[1].search_messenges_to(me, user)
+        db = self._get_database(database_id)
+        if db:
+            return db.search_messenges_to(me, user)
+
+        # TODO: determinar el return type de db.search_messenges_to
+        return None
 
     def delete_messenges(self, id_messenger: int, database_id: int):
-        if database_id == -1:
-            return self.database.delete_messenges(id_messenger)
-        if self.predecessor_replica[0] == database_id:
-            return self.predecessor_replica[1].delete_messenges(id_messenger)
-        return self.second_predecessor_replica[1].delete_messenges(id_messenger)
-        
+        db = self._get_database(database_id)
+        if db:
+            return db.delete_messenges(id_messenger)
+
+        return False
+
     def copy_database(self, source: DataBaseUser, database_id: int):
         if database_id == -1:
             return self.database.copy_database(source)
