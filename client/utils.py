eef8902782adf5f03050f86ed91ab827614807f0
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-09 12:37:47 -0600

    Update utils.py

--- client/utils.py
+++ client/utils.py
@@ -83,7 +83,7 @@ def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messe
         server_node_data.id = generate_id(
             f"{inf_nodo.ip}:{inf_nodo.port}", capacity)
 
-        server_node_data.add_messenger(source, destiny, messenge,-1)
+        server_node_data.add_messenges(source, destiny, messenge,-1)
     except:
         return False
     try:
@@ -96,7 +96,7 @@ def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messe
             server_successor.id = generate_id(
             f"{dict_successor.ip}:{dict_successor.port}", capacity)
 
-            server_successor.add_messenger(
+            server_successor.add_messenges(
                 source, destiny, messenge, server_node_data.id)
 
         # replicar la informacion del usuario en el nodos antecesor
@@ -109,7 +109,7 @@ def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messe
                 server_successor_successor.id = generate_id(
                     f"{server_successor_successor.ip}:{server_successor_successor.port}", capacity)
 
-                server_successor_successor.add_messenger(
+                server_successor_successor.add_messenges(
                             source, destiny, messenge, server_node_data.id)
     except:
         return False
@@ -117,7 +117,7 @@ def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messe
 
 def task_receive_message(nickname: str, data: DataBaseClient, server_node_data:RemoteEntityNode, server_successor: Union[RemoteEntityNode,None], server_successor_successor: Union[RemoteEntityNode,None,bool]):
     # Lista de tupla de quien lo envio, value
-    messenges = server_node_data.search_messenger_to(nickname,' ',-1)
+    messenges = server_node_data.search_messenges_to(nickname,'',-1)
     if server_successor is not None:
         server_successor.delete_messenges_to(nickname," ",server_node_data.id)
     if server_successor_successor is not None and server_successor_successor is not False and  server_successor_successor is not True:
