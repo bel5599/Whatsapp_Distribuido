50e676bddb5593fddd1a404ff0d576dc632e6c03
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-10 13:48:03 -0600

    Update messages.py

--- server/node/routers/messages.py
+++ server/node/routers/messages.py
@@ -7,17 +7,17 @@ from ..models import MessengerModel, SearchMessengerModel, DataBaseModel
 router = APIRouter(prefix="/messages", tags=["messages"])
 
 
-@router.get("/from")
-def search_messages_from(model: SearchMessengerModel, request: Request):
-    node: EntityNode = request.state.node
+# @router.get("/from")
+# def search_messages_from(model: SearchMessengerModel, request: Request):
+#     node: EntityNode = request.state.node
 
-    try:
-        result = node.search_messages_from(
-            model.source, model.destiny, model.database_id)
-    except:
-        raise HTTPException(status_code=404, detail="messages not found!")
-    else:
-        return [{"user_id_from": user_id_from, "value": value} for (user_id_from, value) in result]
+#     try:
+#         result = node.search_messages_from(
+#             model.source, model.destiny, model.database_id)
+#     except:
+#         raise HTTPException(status_code=404, detail="messages not found!")
+#     else:
+#         return [{"user_id_from": user_id_from, "value": value} for (user_id_from, value) in result]
 
 
 @router.get("/to")
@@ -35,16 +35,16 @@ def search_messages_to(model: SearchMessengerModel, request: Request):
 # ARREGLAR result
 
 
-@router.get("/")
-def get_messages(model: DataBaseModel, request: Request):
-    node: EntityNode = request.state.node
+# @router.get("/")
+# def get_messages(model: DataBaseModel, request: Request):
+#     node: EntityNode = request.state.node
 
-    try:
-        result = node.get_messages(model.database_id)
-    except:
-        raise HTTPException(status_code=404, detail="messages not found!")
-    else:
-        return None
+#     try:
+#         result = node.get_messages(model.database_id)
+#     except:
+#         raise HTTPException(status_code=404, detail="messages not found!")
+#     else:
+#         return None
 
 
 @router.put("/add")
@@ -61,17 +61,17 @@ def add_messages(model: MessengerModel, request: Request):
         return {"success": result}
 
 
-@router.delete("/delete/{id}")
-def delete_messages(id: int, model: DataBaseModel, request: Request):
-    node: EntityNode = request.state.node
+# @router.delete("/delete/{id}")
+# def delete_messages(id: int, model: DataBaseModel, request: Request):
+#     node: EntityNode = request.state.node
 
-    try:
-        result = node.delete_messages(id, model.database_id)
-    except:
-        raise HTTPException(
-            status_code=500, detail="delete messages failed!")
-    else:
-        return {"success": result}
+#     try:
+#         result = node.delete_messages(id, model.database_id)
+#     except:
+#         raise HTTPException(
+#             status_code=500, detail="delete messages failed!")
+#     else:
+#         return {"success": result}
 
 
 @router.delete("/delete/to/{me}")
@@ -87,14 +87,14 @@ def delete_messages_to(me: str, model: DataBaseModel, request: Request):
         return {"success": result}
 
 
-@router.delete("/delete/from/{me}")
-def delete_messages_from(me: str, model: DataBaseModel, request: Request):
-    node: EntityNode = request.state.node
+# @router.delete("/delete/from/{me}")
+# def delete_messages_from(me: str, model: DataBaseModel, request: Request):
+#     node: EntityNode = request.state.node
 
-    try:
-        result = node.delete_messages_from(me, model.database_id)
-    except:
-        raise HTTPException(
-            status_code=500, detail="delete messages failed!")
-    else:
-        return {"success": result}
+#     try:
+#         result = node.delete_messages_from(me, model.database_id)
+#     except:
+#         raise HTTPException(
+#             status_code=500, detail="delete messages failed!")
+#     else:
+#         return {"success": result}
