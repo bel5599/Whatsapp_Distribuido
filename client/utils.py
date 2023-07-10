c1124ae3d8c45d4a0ddf9a040d5164748095bbbf
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-10 13:00:51 -0600

    Update utils.py

--- client/utils.py
+++ client/utils.py
@@ -4,46 +4,46 @@ from data.database_client import DataBaseClient
 from server.node.base_entity_node import BaseEntityNode 
 from typing import Union
 
-def replication_user(inf_nodo: BaseEntityNode, nickname: str, password: str, ip: str, port: str):
+def register_user(inf_nodo: BaseEntityNode, nickname: str, password: str, ip: str, port: str):
     try:
         # nodo que va a guardar la informacion del user
         server_node_data = RemoteEntityNode(-1,inf_nodo.ip, inf_nodo.port)
         capacity = server_node_data.network_capacity()
         server_node_data.id = generate_id(
             f"{inf_nodo.ip}:{inf_nodo.port}", capacity)
-
-        server_node_data.add_user(nickname, password, ip, port,-1)
+        
+        # replica si puedes
+        return server_node_data.add_user(nickname, password, ip, port,-1)
     except:
-        return False, False
-    try:
-        # replicar la informacion del usuario en el nodo sucesor
-        dict_successor = server_node_data.successor()
-        if dict_successor is not None:
-            server_successor = RemoteEntityNode(-1,
-                                            dict_successor.ip, dict_successor.port)
-            server_successor.id = generate_id(
-            f"{dict_successor.ip}:{dict_successor.port}", capacity)
+        return False
+    #     return False, False
+    # try:
+    #     # replicar la informacion del usuario en el nodo sucesor
+    #     dict_successor = server_node_data.successor()
+    
+    #     if dict_successor is not None:
+    #         server_successor = RemoteEntityNode(-1,
+    #                                         dict_successor.ip, dict_successor.port)
+    #         server_successor.id = generate_id(
+    #         f"{dict_successor.ip}:{dict_successor.port}", capacity)
 
-            server_successor.add_user(
-            nickname, password, ip, port, server_node_data.id)
-
-            # replicar la informacion del usuario en el nodo sucesor del sucesor 
-            dict_successor_successor = server_successor.successor()
-            if dict_successor_successor is not None:
-                server_successor_successor = RemoteEntityNode(
-                        -1, dict_successor_successor.ip, dict_successor_successor.port)
+    #         # server_successor.add_user(
+    #         # nickname, password, ip, port, server_node_data.id)
+    #         dict_successor_successor = server_successor.successor()
+    #         if dict_successor_successor is not None:
+    #             # server_successor_successor = RemoteEntityNode(
+    #             #         -1, dict_successor_successor.ip, dict_successor_successor.port)
                 
-                server_successor_successor.id = generate_id(
-                        f"{server_successor_successor.ip}:{server_successor_successor.port}", capacity)
+    #             # server_successor_successor.id = generate_id(
+    #             #         f"{server_successor_successor.ip}:{server_successor_successor.port}", capacity)
 
-                server_successor_successor.add_user(
-                    nickname, password, ip, port, server_node_data.id)
-                
-                return dict_successor, dict_successor_successor
-            return server_successor,None
-        return None,None  
-    except:
-        return False, False
+    #             # server_successor_successor.add_user(
+    #             #     nickname, password, ip, port, server_node_data.id)                
+    #             return dict_successor, dict_successor_successor
+    #         return dict_successor,None
+    #     return None,None  
+    # except:
+    #     return False, False
 
 def get_entity_data(inf_nodo: BaseEntityNode):
     # nodo que va a guardar la informacion del user
@@ -74,7 +74,7 @@ def get_entity_data(inf_nodo: BaseEntityNode):
         return False, False, False
 
 
-def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messenge:str):
+def add_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messenge:str):
     try:
         # nodo que va a guardar la informacion del user
         server_node_data = RemoteEntityNode(-1,
@@ -83,45 +83,46 @@ def replication_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messe
         server_node_data.id = generate_id(
             f"{inf_nodo.ip}:{inf_nodo.port}", capacity)
 
-        server_node_data.add_messenges(source, destiny, messenge,-1)
+        server_node_data.add_messages(source, destiny, messenge,-1)
     except:
         return False
-    try:
-        # replicar la informacion del usuario en el nodo sucesor
-        dict_successor = server_node_data.successor()
-        if dict_successor is not None:
-            server_successor = RemoteEntityNode(-1,
-                                            dict_successor.ip, dict_successor.port)
-        # capacity = server_successor.network_capacity()
-            server_successor.id = generate_id(
-            f"{dict_successor.ip}:{dict_successor.port}", capacity)
+    # try:
+    #     # replicar la informacion del usuario en el nodo sucesor
+    #     dict_successor = server_node_data.successor()
+    #     if dict_successor is not None:
+    #         server_successor = RemoteEntityNode(-1,
+    #                                         dict_successor.ip, dict_successor.port)
+        
+    #         server_successor.id = generate_id(
+    #         f"{dict_successor.ip}:{dict_successor.port}", capacity)
 
-            server_successor.add_messenges(
-                source, destiny, messenge, server_node_data.id)
+    #         server_successor.add_messenges(
+    #             source, destiny, messenge, server_node_data.id)
 
-        # replicar la informacion del usuario en el nodos antecesor
-            dict_successor_successor = server_successor.successor()
-            if dict_successor_successor is not None:
+    #     # replicar la informacion del usuario en el nodos antecesor
+    #         dict_successor_successor = server_successor.successor()
+    #         if dict_successor_successor is not None:
                 
-                server_successor_successor = RemoteEntityNode(
-                    -1, dict_successor_successor.ip, dict_successor_successor.port)
-        # capacity = server_successor_successor.network_capacity()
-                server_successor_successor.id = generate_id(
-                    f"{server_successor_successor.ip}:{server_successor_successor.port}", capacity)
+    #             server_successor_successor = RemoteEntityNode(
+    #                 -1, dict_successor_successor.ip, dict_successor_successor.port)
+    #     # capacity = server_successor_successor.network_capacity()
+    #             server_successor_successor.id = generate_id(
+    #                 f"{server_successor_successor.ip}:{server_successor_successor.port}", capacity)
 
-                server_successor_successor.add_messenges(
-                            source, destiny, messenge, server_node_data.id)
-    except:
-        return False
+    #             server_successor_successor.add_messenges(
+    #                         source, destiny, messenge, server_node_data.id)
+    # except:
+    #     return False
 
 
 def task_receive_message(nickname: str, data: DataBaseClient, server_node_data:RemoteEntityNode, server_successor: Union[RemoteEntityNode,None], server_successor_successor: Union[RemoteEntityNode,None,bool]):
     # Lista de tupla de quien lo envio, value
-    messenges = server_node_data.search_messenges_to(nickname,'',-1)
-    if server_successor is not None:
-        server_successor.delete_messenges_to(nickname,server_node_data.id)
-    if server_successor_successor is not None and server_successor_successor is not False and  server_successor_successor is not True:
-        server_successor_successor.delete_messenges_to(nickname,server_node_data.id)
-
+    messenges = server_node_data.search_messages_to(nickname,'',-1)
+    # if server_successor is not None:
+    #     server_successor.delete_messenges_to(nickname,server_node_data.id)
+    # if server_successor_successor is not None and server_successor_successor is not False and  server_successor_successor is not True:
+    #     server_successor_successor.delete_messenges_to(nickname,server_node_data.id)
+    server_node_data.delete_messages_to(nickname,-1)
+    
     for messenge in messenges:
         data.add_messenges(messenge[0], nickname, messenge[1])
