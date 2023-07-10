aa016c46605cf17104235381e50119c22db934f8
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-07-10 12:47:59 -0600

    messages

--- server/node/routers/messages.py
+++ server/node/routers/messages.py
@@ -4,31 +4,31 @@ from ..entity_node import EntityNode
 from ..models import MessengerModel, SearchMessengerModel, DataBaseModel
 
 
-router = APIRouter(prefix="/messenges", tags=["messenges"])
+router = APIRouter(prefix="/messages", tags=["messages"])
 
 
 @router.get("/from")
-def search_messenges_from(model: SearchMessengerModel, request: Request):
+def search_messages_from(model: SearchMessengerModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.search_messenges_from(
+        result = node.search_messages_from(
             model.source, model.destiny, model.database_id)
     except:
-        raise HTTPException(status_code=404, detail="messenges not found!")
+        raise HTTPException(status_code=404, detail="messages not found!")
     else:
         return [{"user_id_from": user_id_from, "value": value} for (user_id_from, value) in result]
 
 
 @router.get("/to")
-def search_messenges_to(model: SearchMessengerModel, request: Request):
+def search_messages_to(model: SearchMessengerModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.search_messenges_to(
+        result = node.search_messages_to(
             model.source, model.destiny, model.database_id)
     except:
-        raise HTTPException(status_code=404, detail="messenges not found!")
+        raise HTTPException(status_code=404, detail="messages not found!")
     else:
         return [{"user_id_from": user_id_from, "value": value} for (user_id_from, value) in result]
 
@@ -42,59 +42,59 @@ def get_messages(model: DataBaseModel, request: Request):
     try:
         result = node.get_messages(model.database_id)
     except:
-        raise HTTPException(status_code=404, detail="messenges not found!")
+        raise HTTPException(status_code=404, detail="messages not found!")
     else:
         return None
 
 
 @router.put("/add")
-def add_messenges(model: MessengerModel, request: Request):
+def add_messages(model: MessengerModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.add_messenges(
+        result = node.add_messages(
             model.source, model.destiny, model.value, model.database_id)
     except:
         raise HTTPException(
-            status_code=500, detail="add messenger failed!")
+            status_code=500, detail="add messages failed!")
     else:
         return {"success": result}
 
 
 @router.delete("/delete/{id}")
-def delete_messenges(id: int, model: DataBaseModel, request: Request):
+def delete_messages(id: int, model: DataBaseModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.delete_messenges(id, model.database_id)
+        result = node.delete_messages(id, model.database_id)
     except:
         raise HTTPException(
-            status_code=500, detail="delete messenger failed!")
+            status_code=500, detail="delete messages failed!")
     else:
         return {"success": result}
 
 
 @router.delete("/delete/to/{me}")
-def delete_messenges_to(me: str, model: DataBaseModel, request: Request):
+def delete_messages_to(me: str, model: DataBaseModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.delete_messenges_to(me, model.database_id)
+        result = node.delete_messages_to(me, model.database_id)
     except:
         raise HTTPException(
-            status_code=500, detail="delete messenger failed!")
+            status_code=500, detail="delete messages failed!")
     else:
         return {"success": result}
 
 
 @router.delete("/delete/from/{me}")
-def delete_messenges_from(me: str, model: DataBaseModel, request: Request):
+def delete_messages_from(me: str, model: DataBaseModel, request: Request):
     node: EntityNode = request.state.node
 
     try:
-        result = node.delete_messenges_from(me, model.database_id)
+        result = node.delete_messages_from(me, model.database_id)
     except:
         raise HTTPException(
-            status_code=500, detail="delete messenger failed!")
+            status_code=500, detail="delete messages failed!")
     else:
         return {"success": result}
