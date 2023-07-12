b984148c73c865f240286f80b273dd48fbd973e2
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-11 21:14:02 -0400

    format & remove unused imports

--- service_app.py
+++ service_app.py
@@ -1,15 +1,13 @@
 # levanta la interfaz del cliente
 if __name__ == "__main__":
     import uvicorn
-    import threading
-    import time
     from typer import Typer
     from client.client import service
 
     typer_app = Typer()
 
     @typer_app.command()
-    def run(ip:str):
-        uvicorn.run(service,host=ip, port=int('8765'))
+    def run(ip: str):
+        uvicorn.run(service, host=ip, port=int('8765'))
 
     typer_app()
