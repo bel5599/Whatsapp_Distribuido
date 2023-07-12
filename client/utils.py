f105cc730284f4e7736cb1173e5ac867df672d97
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-12 00:55:19 -0400

    format, remove unused imports and add FIXED_PORT constant

--- client/utils.py
+++ client/utils.py
@@ -1,40 +1,42 @@
-from server.node.remote_entity_node import RemoteEntityNode
-from server.util import generate_id
 from data.database_client import DataBaseClient
-from server.node.base_entity_node import BaseEntityNode 
-from typing import Union
+from server.node.base_entity_node import BaseEntityNode
+
+FIXED_PORT = 8765
+
 
 def register_user(inf_nodo: BaseEntityNode, nickname: str, password: str, ip: str, port: str):
     try:
-        result = inf_nodo.add_user(nickname, password, ip, port,-1)
+        result = inf_nodo.add_user(nickname, password, ip, port, -1)
         return result
     except:
         return False
-    
+
+
 def get_entity_data(inf_nodo: BaseEntityNode):
     try:
         dict_successor = inf_nodo.successor()
         if dict_successor is not None:
             dict_successor_successor = dict_successor.successor()
-            if dict_successor_successor is not None:    
-                return inf_nodo,dict_successor,dict_successor_successor
-            return inf_nodo,dict_successor,None
-        return inf_nodo,None,None  
+            if dict_successor_successor is not None:
+                return inf_nodo, dict_successor, dict_successor_successor
+            return inf_nodo, dict_successor, None
+        return inf_nodo, None, None
     except:
         return False, False, False
 
 
-def add_messenge(inf_nodo:BaseEntityNode, source:str, destiny:str, messenge:str):
+def add_messenge(inf_nodo: BaseEntityNode, source: str, destiny: str, messenge: str):
     try:
-        inf_nodo.add_messages(source, destiny, messenge,-1)
+        inf_nodo.add_messages(source, destiny, messenge, -1)
     except:
         return False
-   
-def task_receive_message(nickname: str, data: DataBaseClient, server_node_data:BaseEntityNode):
+
+
+def task_receive_message(nickname: str, data: DataBaseClient, server_node_data: BaseEntityNode):
     # Lista de tupla de quien lo envio, value
-    messenges = server_node_data.search_messages_to(nickname,-1)
+    messenges = server_node_data.search_messages_to(nickname, -1)
     # replica a los sucesores desd eel entity
-    server_node_data.delete_messages_to(nickname,-1)
-    
+    server_node_data.delete_messages_to(nickname, -1)
+
     for messenge in messenges:
         data.add_messages(messenge[0], nickname, messenge[1])
