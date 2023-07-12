56765cbfac21b5322d8786b4e98adc3b167ed4c4
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-12 00:57:05 -0400

    service up with network ip and fixed port

--- service_app.py
+++ service_app.py
@@ -3,11 +3,14 @@ if __name__ == "__main__":
     import uvicorn
     from typer import Typer
     from client.client import service
+    from client.utils import FIXED_PORT
+    from shared import get_ip
 
     typer_app = Typer()
 
     @typer_app.command()
-    def run(ip: str):
-        uvicorn.run(service, host=ip, port=int('8765'))
+    def run():
+        ip = get_ip()
+        uvicorn.run(service, host=ip, port=int(FIXED_PORT))
 
     typer_app()
