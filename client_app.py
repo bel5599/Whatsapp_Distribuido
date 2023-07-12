a9be5a9763c9e86fa1648150673eacc8e168fb23
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-12 00:56:41 -0400

    server up with local ip

--- client_app.py
+++ client_app.py
@@ -5,13 +5,12 @@ if __name__ == "__main__":
     import time
     from typer import Typer
     from client.client import client_interface, client
+    from shared import LOCAL_IP
 
     typer_app = Typer()
 
     @typer_app.command()
-    def run(ip: str, port: str = '9000'):
-        client.ip = ip
-        client.port = '8765'
+    def run(port: str = '9000'):
         # Creacion de un hilo para este servicio
 
         def update():
@@ -21,6 +20,6 @@ if __name__ == "__main__":
         # Levantar el hilo
         stabilize_task.start()
         # Correr el cliente
-        uvicorn.run(client_interface, host=ip, port=int(port))
+        uvicorn.run(client_interface, host=LOCAL_IP, port=int(port))
 
     typer_app()
