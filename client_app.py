ac60fc4436a2477cbec05e1938fdf258d2e9025f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-11 21:14:25 -0400

    format and use daemon thread

--- client_app.py
+++ client_app.py
@@ -1,26 +1,26 @@
-
 # levanta la interfaz del cliente
 if __name__ == "__main__":
     import uvicorn
     import threading
     import time
     from typer import Typer
-    from client.client import client_interface,client
+    from client.client import client_interface, client
 
     typer_app = Typer()
 
     @typer_app.command()
-    def run(ip:str,port:str ='9000'):
+    def run(ip: str, port: str = '9000'):
         client.ip = ip
         client.port = '8765'
         # Creacion de un hilo para este servicio
+
         def update():
             time.sleep(1)
             client.update_servers()
-        stabilize_task = threading.Thread(target=update)    
+        stabilize_task = threading.Thread(target=update, daemon=True)
         # Levantar el hilo
         stabilize_task.start()
         # Correr el cliente
-        uvicorn.run(client_interface,host=ip, port=int(port))
+        uvicorn.run(client_interface, host=ip, port=int(port))
 
     typer_app()
