75c62314202fca098a72b99dcdda54b15c8b6548
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-14 13:01:41 -0400

    use fixed client port

--- service_app.py
+++ service_app.py
@@ -3,14 +3,13 @@ if __name__ == "__main__":
     import uvicorn
     from typer import Typer
     from client.client import service
-    from client.utils import FIXED_PORT
-    from shared import get_ip
+    from shared import get_ip, CLIENT_PORT
 
     typer_app = Typer()
 
     @typer_app.command()
     def run():
         ip = get_ip()
-        uvicorn.run(service, host=ip, port=int(FIXED_PORT))
+        uvicorn.run(service, host=ip, port=int(CLIENT_PORT))
 
     typer_app()
