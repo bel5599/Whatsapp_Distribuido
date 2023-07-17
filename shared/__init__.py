ca67fae024269c1e31bca23b737b073cb95bb6bb
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-17 19:46:09 -0400

    add inject_to_state function

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,8 +1,9 @@
 import socket
+from fastapi import FastAPI, Request
 
 
 LOCAL_IP = "127.0.0.1"
-SERVICE_PORT ="8480"
+SERVICE_PORT = "8480"
 CLIENT_PORT = "9050"
 SERVER_PORT = "4173"
 
@@ -24,3 +25,11 @@ def get_ip(local=False):
             ip = LOCAL_IP
 
     return ip
+
+
+def inject_to_state(app: FastAPI, name: str, obj):
+    async def middleware(request: Request, call_next):
+        setattr(request.state, name, obj)
+        return await call_next(request)
+
+    app.middleware("http")(middleware)
