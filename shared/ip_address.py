51cc609da15088b99905ae101b7a68e04958a26a
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-07-18 11:33:35 -0400

    impl IPAddress

---
+++ shared/ip_address.py
@@ -0,0 +1,21 @@
+class IPAddress:
+    def __init__(self, *numbers: int):
+        assert len(numbers) == 4 and all(
+            0 <= n < 256 for n in numbers), "Invalid IP address"
+
+        self._numbers = numbers
+
+    def __repr__(self):
+        return ".".join([str(n) for n in self._numbers])
+
+    def to_string(self):
+        return self.__repr__()
+
+    def __and__(self, other: "IPAddress"):
+        return IPAddress(*[a & b for a, b in zip(self._numbers, other._numbers)])
+
+    def __or__(self, other: "IPAddress"):
+        return IPAddress(*[a | b for a, b in zip(self._numbers, other._numbers)])
+
+    def __invert__(self):
+        return IPAddress(*[~n+256 for n in self._numbers])
